d2df611d16cc6d1d84af50deedec2512ec2a316f
Utishnik <yrik.kokoyto.00@bk.ru>
2026-02-22 02:23:45 +0300

    add more impl

--- src/async_net/async_native_tls_net.rs
+++ src/async_net/async_native_tls_net.rs
@@ -11,6 +11,8 @@ use futures_io::{
     Result as IoResult,
 };
 
+use crate::net_err;
+
 enum InnerAsyncNetworkStream {
     #[cfg(feature = "tokio")]
     Tokio1Tcp(Box<dyn AsyncTokioStream>),
@@ -63,5 +65,61 @@ pub async fn connect_tokio<T: Tokio1ToSocketAddrs>(
         local_addr: Option<IpAddr>,
     ) -> Result<AsyncNetworkStream, Error>
 {
+        async fn try_connect<T: Tokio1ToSocketAddrs>(
+            server: T,
+            timeout: Option<Duration>,
+            local_addr: Option<IpAddr>,
+        ) -> Result<TokioTcpStream, Error> {
+            let addrs = tokio::net::lookup_host(server)
+                .await
+                .map_err(crate::net_err::connection)?
+                .filter(|resolved_addr| resolved_address_filter(resolved_addr, local_addr));
 
+            let mut last_err = None;
+
+            for addr in addrs {
+                let socket = match addr.ip() {
+                    IpAddr::V4(_) => TokioTcpSocket::new_v4(),
+                    IpAddr::V6(_) => TokioTcpSocket::new_v6(),
+                }
+                .map_err(crate::net_err::connection)?;
+                if let Some(local_addr) = local_addr {
+                    socket
+                        .bind(SocketAddr::new(local_addr, 0))
+                        .map_err(crate::net_err::connection)?;
+                }
+
+                let connect_future = socket.connect(addr);
+                if let Some(timeout) = timeout {
+                    match tokio::time::timeout(timeout, connect_future).await {
+                        Ok(Ok(stream)) => return Ok(stream),
+                        Ok(Err(err)) => last_err = Some(err),
+                        Err(_) => {
+                            last_err = Some(io::Error::new(
+                                io::ErrorKind::TimedOut,
+                                "connection timed out",
+                            ));
+                        }
+                    }
+                } else {
+                    match connect_future.await {
+                        Ok(stream) => return Ok(stream),
+                        Err(err) => last_err = Some(err),
+                    }
+                }
+            }
+
+            Err(match last_err {
+                Some(last_err) => crate::net_err::connection(last_err),
+                None => crate::net_err::connection("could not resolve to any supported address"),
+            })
+        }
+
+        let tcp_stream = try_connect(server, timeout, local_addr).await?;
+        let mut stream =
+            AsyncNetworkStream::new(InnerAsyncNetworkStream::Tokio1Tcp(Box::new(tcp_stream)));
+        if let Some(tls_parameters) = tls_parameters {
+            stream.upgrade_tls(tls_parameters).await?;
+        }
+        Ok(stream)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,7 @@ pub mod rustls_crypto;
 pub mod tls_common;
 pub mod async_net;
 pub mod net;
+pub mod net_err;
+mod response;
+use std::error::Error as StdError;
+pub(crate) type BoxError = Box<dyn StdError + Send + Sync>;
